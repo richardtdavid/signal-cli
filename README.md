eb3cad0b332081fcaf4c8edf88ca6e217870d0a6
richarddavid <driichard@gmail.com>
2021-02-23 18:07:52 -0600

    setup node-notifer server

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # signal-cli
+
+![]('./notifier/signal-icon.png')
+
---
+++ notifier/notifier.ts
@@ -0,0 +1,50 @@
+import * as path from "path";
+import express from "express";
+import bodyParser from "body-parser";
+import notifier, { NotificationMetadata } from "node-notifier";
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.use(bodyParser.json());
+
+app.get("/health", (req, res) =>
+  res.status(200).send({
+    date: new Date(),
+    status: 200,
+    msg: "Signal Notifier Server Healthy",
+  })
+);
+app.post("/notify", (req, res) => {
+  notify(req.body, (reply: NotificationMetadata) => res.send(reply));
+});
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
+
+function notify({ title, msg }: Notification, cb: any) {
+  console.log({ title, msg });
+
+  notifier.notify(
+    {
+      title: title || "Signal",
+      message: msg || "Signal",
+      icon: path.join(__dirname, "signal-icon.png"),
+      sound: true,
+      wait: true,
+      reply: true,
+      closeLabel: "Done!",
+      timeout: 15,
+    },
+    (err, response, metadata) => {
+      console.log(response, err);
+      cb(<NotificationMetadata>metadata);
+    }
+  );
+}
+
+type CallBack = (reply: NotificationMetadata) => void;
+
+interface Notification {
+  title: string;
+  msg: string;
+}
